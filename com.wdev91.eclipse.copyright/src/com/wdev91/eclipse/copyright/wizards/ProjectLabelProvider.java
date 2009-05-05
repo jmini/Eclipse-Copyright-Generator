e912aae07ed2901b8db063387ff07601fc3009bc
Eric Wuillai <ewuillai@sourceforge.net>
2009-05-05 22:03:19 +0000

    New feature: project level settings for copyright and header formats
    git-svn-id: svn://svn.code.sf.net/p/eclipsecopyrigh/code/trunk@18 3b2ae194-4b46-0410-b15f-8b9759acbdba

---
+++ com.wdev91.eclipse.copyright/src/com/wdev91/eclipse/copyright/wizards/ProjectLabelProvider.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2009 Eric Wuillai.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Eric Wuillai - initial API and implementation
+ ******************************************************************************/
+package com.wdev91.eclipse.copyright.wizards;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
+
+import com.wdev91.eclipse.copyright.model.CopyrightManager;
+import com.wdev91.eclipse.copyright.model.ProjectPreferences;
+
+class ProjectLabelProvider implements ILabelProvider {
+  private static final String COPYRIGHT_LABEL = "  (C)"; //$NON-NLS-1$
+
+  private ILabelProvider workbenchProvider;
+
+  ProjectLabelProvider() {
+    workbenchProvider = WorkbenchLabelProvider.getDecoratingWorkbenchLabelProvider();
+  }
+
+  public Image getImage(Object element) {
+    return workbenchProvider.getImage(element);
+  }
+
+  public String getText(Object element) {
+    String text = workbenchProvider.getText(element);
+    if ( CopyrightManager.getProjectPreferences((IProject) element) != ProjectPreferences.NO_PREFS ) {
+      return text + COPYRIGHT_LABEL;
+    } else {
+      return text;
+    }
+  }
+
+  public void addListener(ILabelProviderListener listener) {
+    workbenchProvider.addListener(listener);
+  }
+
+  public void dispose() {
+    workbenchProvider.dispose();
+  }
+
+  public boolean isLabelProperty(Object element, String property) {
+    return workbenchProvider.isLabelProperty(element, property);
+  }
+
+  public void removeListener(ILabelProviderListener listener) {
+    workbenchProvider.removeListener(listener);
+  }
+}
